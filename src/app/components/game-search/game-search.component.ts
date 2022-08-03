637f435625e60b57bc3b31cefa57055d6c78c487
tbergeson <talmage.bergeson3761@stu.mtec.edu>
2022-08-03 10:14:32 -0600

    completed unit #7

--- src/app/components/game-search/game-search.component.ts
+++ src/app/components/game-search/game-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/app/interfaces/game';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-game-search',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-search.component.scss']
 })
 export class GameSearchComponent implements OnInit {
+  searchText: string = '';
+  games: Game[] = [];
 
-  constructor() { }
+  constructor(
+    private gameService: GameService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  search() {
+    this.gameService.searchByName(this.searchText).subscribe(games => this.games = games);
+  }
 }
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
@@ -14,13 +17,21 @@ import {MatIconModule} from '@angular/material/icon';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ],
   exports: [
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
